70f72639b402e4371f07aa871ca4aaa607c9f44c
Gabi Lapadat <lapadatrobert123@gmail.com>
2022-07-18 04:09:48 +0300

    - Added login view and controller - Added register view and controller - Referenced the controller and view inside the nav bar links

---
+++ src/WebApp/WebShop.MVC/Controllers/RegisterController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebShop.MVC.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly ILogger<RegisterController> _logger;
+
+        public RegisterController(ILogger<RegisterController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
