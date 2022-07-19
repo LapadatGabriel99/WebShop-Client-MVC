26dfb194252bb4c1edfd9288b7de2f82de301048
Gabi Lapadat <lapadatrobert123@gmail.com>
2022-07-20 00:21:01 +0300

    - Added register action. - In case of model not being valid we redirect to Index, to display errors.

--- src/WebApp/WebShop.MVC/Controllers/RegisterController.cs
+++ src/WebApp/WebShop.MVC/Controllers/RegisterController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebShop.MVC.Models;
 
 namespace WebShop.MVC.Controllers
 {
@@ -11,9 +12,27 @@ namespace WebShop.MVC.Controllers
             _logger = logger;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Index(RegisterViewModel registerViewModel)
+        {
+            return View(registerViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(RegisterViewModel registerViewModel)
+        {
+            if (ModelState.IsValid is false)
+            {
+                return View("Index", registerViewModel);
+            }
+
+            return RedirectToAction("Index", "Login");
         }
     }
 }
